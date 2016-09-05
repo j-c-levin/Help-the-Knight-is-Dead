5754994909c2d4116c0657ac67d3d30cf5fd0ae3
joshua levin <joshua.c.levin@gmail.com>
2016-09-05 19:40:55 +0100

    add basic scene, controllers, a player that can move and a bit of air console

---
+++ Assets/Test Level 1/Scripts/AirConsoleManager.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleManager : MonoBehaviour
+{
+	private GameManager m_gameManager;
+
+	void Awake ()
+	{
+		if (AirConsole.instance != null) {
+			SetupAirConsoleEvents ();
+		}
+	}
+
+	void Start ()
+	{
+		m_gameManager = GetComponent<GameManager> ();
+	}
+
+	void SetupAirConsoleEvents ()
+	{
+		AirConsole.instance.onReady += OnReady;
+		AirConsole.instance.onMessage += OnMessage;
+		AirConsole.instance.onConnect += OnConnect;
+		AirConsole.instance.onDisconnect += OnDisconnect;
+		AirConsole.instance.onDeviceStateChange += OnDeviceStateChange;
+		AirConsole.instance.onCustomDeviceStateChange += OnCustomDeviceStateChange;
+		AirConsole.instance.onDeviceProfileChange += OnDeviceProfileChange;
+		AirConsole.instance.onAdShow += OnAdShow;
+		AirConsole.instance.onAdComplete += OnAdComplete;
+		AirConsole.instance.onGameEnd += OnGameEnd;
+		Debug.Log ("Connecting...");
+	}
+
+	void OnReady (string code)
+	{
+		Debug.Log ("Ready: " + code);
+	}
+
+	void OnMessage (int from, JToken data)
+	{ 
+		if ((object)data ["joystick-left"] != null) {
+			ReceivedMovement (from, data);
+			return;
+		}
+
+		if ((object)data ["action"] != null) {
+			ReceivedAction (from, data);
+			return;
+		}
+
+		Debug.LogError ("unknown input: " + data.ToString ());
+	}
+
+	void ReceivedMovement (int from, JToken data)
+	{
+		if (!(bool)data ["joystick-left"] ["pressed"]) { 
+			m_gameManager.SetPlayerMovement (from, 0, 0);
+			return;
+		}
+
+		//Update player movement
+		m_gameManager.SetPlayerMovement (from, (float)data ["joystick-left"] ["message"] ["x"], -(float)data ["joystick-left"] ["message"] ["y"]);
+	}
+
+	void ReceivedAction (int from, JToken data)
+	{
+		m_gameManager.SetPlayerAction (from, (bool)data ["special"] ["pressed"]);
+	}
+
+	void OnConnect (int device_id)
+	{
+		Debug.Log ("New device connected: " + device_id);
+		m_gameManager.OnPlayerConnected (device_id);
+	}
+
+	void OnDisconnect (int device_id)
+	{
+		Debug.LogError ("Device disconnected and this is not handled");
+	}
+
+	void OnDeviceStateChange (int device_id, JToken data)
+	{
+
+	}
+
+	void OnCustomDeviceStateChange (int device_id, JToken custom_data)
+	{
+
+	}
+
+	void OnDeviceProfileChange (int device_id)
+	{
+
+	}
+
+	void OnAdShow ()
+	{
+
+	}
+
+	void OnAdComplete (bool adWasShown)
+	{
+
+	}
+
+	void OnGameEnd ()
+	{
+		Debug.Log ("OnGameEnd is called");
+	}
+
+	void OnDestroy ()
+	{
+		// unregister events
+		if (AirConsole.instance != null) {
+			AirConsole.instance.onReady -= OnReady;
+			AirConsole.instance.onMessage -= OnMessage;
+			AirConsole.instance.onConnect -= OnConnect;
+			AirConsole.instance.onDisconnect -= OnDisconnect;
+			AirConsole.instance.onDeviceStateChange -= OnDeviceStateChange;
+			AirConsole.instance.onCustomDeviceStateChange -= OnCustomDeviceStateChange;
+			AirConsole.instance.onAdShow -= OnAdShow;
+			AirConsole.instance.onAdComplete -= OnAdComplete;
+			AirConsole.instance.onGameEnd -= OnGameEnd;
+		}
+	}
+}
---
+++ Assets/Test Level 1/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+﻿#define local_testing
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject PlayerPrefab;
+
+	private List<Rigidbody2D> m_players;
+	private List<float[]> m_playerValues;
+
+	private enum PlayerValues
+	{
+		DEVICE_ID = 0,
+		HORIZONTAL_MOVEMENT = 1,
+		VERTICAL_MOVEMENT = 2
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_players = new List<Rigidbody2D> ();
+		m_playerValues = new List<float[]> ();
+
+		#if local_testing
+		OnPlayerConnected (200);
+		#endif
+	}
+
+	public void OnPlayerConnected (int deviceID)
+	{
+		GameObject newPlayer = (GameObject)GameObject.Instantiate (PlayerPrefab, Vector3.zero, Quaternion.identity);
+		m_players.Add (newPlayer.GetComponent<Rigidbody2D> ());
+		m_playerValues.Add (new float[] { deviceID, 0.0f, 0.0f }); 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		#if local_testing
+
+		#endif
+		UpdatePlayerMovement ();
+
+	}
+
+	void UpdatePlayerMovement ()
+	{
+		#if local_testing
+		m_playerValues [0] [(int)PlayerValues.HORIZONTAL_MOVEMENT] = Input.GetAxisRaw ("Horizontal");
+		m_playerValues [0] [(int)PlayerValues.VERTICAL_MOVEMENT] = Input.GetAxisRaw ("Vertical");
+		#endif
+
+		for (int i = 0; i < m_players.Count; i++) {
+			Vector2 playerMovement = new Vector2 (m_playerValues [i] [(int)PlayerValues.HORIZONTAL_MOVEMENT], m_playerValues [i] [(int)PlayerValues.VERTICAL_MOVEMENT]);
+			m_players [i].AddForce (playerMovement, ForceMode2D.Impulse);
+		}
+	}
+
+	void LocalTesting() {
+
+	}
+
+	public void SetPlayerMovement (int sender, float horizontalMovement, float verticalMovement)
+	{
+		for (int i = 0; i < m_players.Count; i++) {
+			if (m_playerValues [i] [(int)PlayerValues.DEVICE_ID] == sender) {
+				m_playerValues [i] [(int)PlayerValues.HORIZONTAL_MOVEMENT] = horizontalMovement;
+				m_playerValues [i] [(int)PlayerValues.VERTICAL_MOVEMENT] = verticalMovement;
+				return;
+			}
+		}
+	}
+
+	public void SetPlayerAction (int sender, bool pressed)
+	{
+		for (int i = 0; i < m_players.Count; i++) {
+				//Handle action here
+				return;
+			}
+		}
+	}
+}
