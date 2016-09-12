0990fb0619123e9ca47830edcdff14e9874cc0ae
joshua levin <joshua.c.levin@gmail.com>
2016-09-12 23:39:18 +0100

    start wave system

--- Assets/Test Level 1/Scripts/EnemySpawner.cs
+++ Assets/Test Level 1/Scripts/EnemySpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using HKD_1;
 
 namespace HKD_1
@@ -14,11 +15,71 @@ namespace HKD_1
 		private float m_maxSpawnTime = 4;
 		private float m_currentSpawnTime;
 
+		List<Wave> waves;
+
+		private struct Wave
+		{
+			public Set[] sets;
+		}
+
+		private struct Set
+		{
+			public int[] units;
+		}
+
 		// Use this for initialization
 		void Start ()
 		{
 			m_spawners = GameObject.FindGameObjectsWithTag ("Enemy Spawner");
 			RandomiseSpawnTime ();
+
+			//SetUpWaves ();
+		}
+
+		/*private void SetUpWaves ()
+		{
+			Set set1;
+			Set set2;
+			Set set3;
+			Set set4;
+			Set set5;
+
+			set1 = NewSet (2, 3);
+			set2 = NewSet (1, 2);
+			set3 = NewSet (2, 2);
+			Wave wave1 = NewWave (set1, set2, set3);
+			waves.Add (wave1);
+
+			set1 = NewSet (3, 3);
+			set2 = NewSet (2, 4);
+			set3 = NewSet (2, 3);
+			Wave wave2 = NewWave (set1, set2, set3);
+
+			set1 = NewSet (1, 3);
+			set2 = NewSet (2, 3);
+			set3 = NewSet (4, 4);
+			set4 = NewSet (5, 4);
+			Wave wave3 = NewWave (set1, set2, set3);
+
+			waves.Add (wave1);
+			waves.Add (wave2);
+			waves.Add (wave3);
+		}*/
+
+		private Wave NewWave (Set[] sets)
+		{
+			Wave response = new Wave ();
+			response.sets = sets;
+
+			return response;
+		}
+
+		private Set NewSet (int[] unitComposition)
+		{
+			Set response = new Set ();
+			response.units = unitComposition;
+
+			return response;
 		}
 
 		void Update ()
